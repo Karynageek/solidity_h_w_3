48e9a5bc24bd0a1511a3acd51b9bbf67b530d37e
Karyna Chelpan <cheplan15@gmail.com>
2021-11-28 23:17:24 +0200

    fix contracts

--- contracts/Sale.sol
+++ contracts/Sale.sol
@@ -2,26 +2,30 @@
 pragma solidity 0.8.10;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "./MySimpleToken.sol";
 
 interface HomeInterface {
     function getStudentList() external view returns (string[] memory students);
 }
 
-contract Sale is MySimpleToken {
+interface ERC20Token {
+    function balanceOf(address owner) external view returns (uint256 balance);
+
+    function transfer(address recipient, uint256 value)
+        external
+        returns (bool success);
+}
+
+contract Sale {
     AggregatorV3Interface internal priceFeed;
     address public home_contract = 0x0E822C71e628b20a35F8bCAbe8c11F274246e64D;
+    address token = 0x489D9b6fF7A2BA6AfC5eae5794038D38C7eC55C3;
+    address aggregator = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
 
     /**
      * Aggregator: ETH/USD in Rinkeby Testnet
      */
-    constructor()
-        public
-        MySimpleToken("MySimpleToken", "MST", 100000000000000000)
-    {
-        priceFeed = AggregatorV3Interface(
-            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
-        );
+    constructor() {
+        priceFeed = AggregatorV3Interface(aggregator);
     }
 
     function buyTokens() public payable {
@@ -30,23 +34,17 @@ contract Sale is MySimpleToken {
 
         uint256 amount = (msg.value * getPrice()) / registeredStudentsLength;
 
-        if (balanceOf(address(this)) < amount) {
+        if (ERC20Token(token).balanceOf(address(this)) >= amount) {
+            ERC20Token(token).transfer(msg.sender, amount);
+        } else {
             msg.sender.call{gas: 3000000, value: msg.value}(
-                "Sorry,there is not enough tokens"
+                "Sorry, there is not enough tokens to buy"
             );
-            return;
         }
-        _transfer(address(this), msg.sender, amount);
     }
 
     function getPrice() public view returns (uint256) {
-        (
-            uint80 roundID,
-            int256 price,
-            uint256 startedAt,
-            uint256 timeStamp,
-            uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
+        (, int256 price, , , ) = priceFeed.latestRoundData();
         return uint256(price / 1e8);
     }
 
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,5 @@
-const Sale = artifacts.require("Sale");
+const MySimpleToken = artifacts.require("MySimpleToken");
 
 module.exports = function (deployer) {
-   deployer.deploy(Sale, "Hello world!");
+   deployer.deploy(MySimpleToken, "MySimpleToken", "MST", 10000000000000);
 };

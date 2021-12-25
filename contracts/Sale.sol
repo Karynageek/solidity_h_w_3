4f5853d570cfaf38cbf6b8bbf1b4af72d7613aa1
Karyna Chelpan <cheplan15@gmail.com>
2021-12-25 19:12:02 +0200

    fix sale contract

--- contracts/Sale.sol
+++ contracts/Sale.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 interface HomeInterface {
-    function getStudentList() external view returns (string[] memory students);
+    function getStudentsList() external view returns (string[] memory students);
 }
 
 interface ERC20Token {
@@ -17,40 +17,54 @@ interface ERC20Token {
 
 contract Sale {
     AggregatorV3Interface internal priceFeed;
-    address public home_contract = 0x0E822C71e628b20a35F8bCAbe8c11F274246e64D;
-    address token = 0x489D9b6fF7A2BA6AfC5eae5794038D38C7eC55C3;
-    address aggregator = 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e;
+    address public home_contract;
+    address token;
 
-    /**
-     * Aggregator: ETH/USD in Rinkeby Testnet
-     */
-    constructor() {
-        priceFeed = AggregatorV3Interface(aggregator);
+    constructor(
+        address _home_contract,
+        address _token,
+        address _chainLinkETHUSDRinkeby
+    ) {
+        home_contract = _home_contract;
+        token = _token;
+        priceFeed = AggregatorV3Interface(_chainLinkETHUSDRinkeby);
+    }
+
+    receive() external payable {
+        buyTokens();
+    }
+
+    fallback() external {
+        buyTokens();
     }
 
     function buyTokens() public payable {
         uint256 registeredStudentsLength = getRegisteredStudentsLength();
-        require(registeredStudentsLength > 0);
+        require(registeredStudentsLength > 0, "Students lenght can't be 0");
+
+        require(msg.value > 0, "Amount of tokens can't be 0");
 
         uint256 amount = (msg.value * getPrice()) / registeredStudentsLength;
 
         if (ERC20Token(token).balanceOf(address(this)) >= amount) {
             ERC20Token(token).transfer(msg.sender, amount);
         } else {
-            msg.sender.call{gas: 3000000, value: msg.value}(
-                "Sorry, there is not enough tokens to buy"
-            );
+            (bool success, ) = msg.sender.call{
+                gas: 300000000000000,
+                value: msg.value
+            }("Sorry, there is not enough tokens to buy");
+            require(success, "External call failed");
         }
     }
 
     function getPrice() public view returns (uint256) {
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-        return uint256(price / 1e8);
+        (, int256 priceETHUSDRinkeby, , , ) = priceFeed.latestRoundData();
+        return uint256(priceETHUSDRinkeby) / (10**priceFeed.decimals());
     }
 
     function getRegisteredStudentsLength() public view returns (uint256) {
         string[] memory students = HomeInterface(home_contract)
-            .getStudentList();
+            .getStudentsList();
 
         return students.length;
     }
